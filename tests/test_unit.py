f34f9b42487e7b0a9f7e8d0559a585df572bc318
Vasilis Gkolemis <ntipakos@gmail.com>
2022-04-26 21:59:58 +0300

    minor fix in unit tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -5,7 +5,7 @@ import sys
 
 # hack to import mdale
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import mdale.dale
+import mdale.utils as utils
 
 
 def test_compute_bin_effects_1():
@@ -14,7 +14,7 @@ def test_compute_bin_effects_1():
     points = np.ones([N]) - .5
     point_effects = np.ones_like(points)*10
     limits = np.array([0, 1, 2.])
-    predict = mdale.dale.compute_bin_effects(points, point_effects, limits)
+    predict = utils.compute_bin_effects(points, point_effects, limits)
 
     # ground-truth
     gt = np.array([10., np.NaN])
@@ -27,7 +27,7 @@ def test_compute_accumulated_effect_1():
     limits = np.array([0, 1, 2.])
     bin_effects = np.array([1., 1.])
     dx = 1.
-    predict = mdale.dale.compute_accumulated_effect(xs, limits, bin_effects, dx)
+    predict = utils.compute_accumulated_effect(xs, limits, bin_effects, dx)
     gt = np.array([0., 0., 0., .5, 1., 1.5, 2., 2., 2.])
     assert np.array_equal(predict, gt)
 
