72cb92474c4516745f56b3006ec6fb85099fb040
Vasilis Gkolemis <vgkolemis@athenarc.gr>
2023-06-06 16:51:26 +0300

    updated example

--- examples/synthetic_repid.py
+++ examples/synthetic_repid.py
@@ -79,41 +79,75 @@ X = dist.generate(N=1000)
 Y = model.predict(X)
 
 
-# check interactions
-h_index = interaction.HIndex(data=X, model=model.predict, nof_instances=950)
-# print(h_index.eval_pairwise(0, 1))
-print(h_index.eval_one_vs_all(0))
-h_index.plot(interaction_matrix=False, one_vs_all=True)
-
-# check interactions with REPID (dPDP based method)
-repid_index = interaction.REPID(data=X, model=model.predict, model_jac=model.jacobian, nof_instances=950)
-print(repid_index.eval_one_vs_all(0))
-repid_index.plot()
-
-
-
-# # find regions
-# reg = pythia.regions.Regions(data=X, model=model.predict, model_jac=model.jacobian, cat_limit=25)
-# reg.search_splits(nof_levels=2, nof_candidate_splits=20, criterion="rhale")
-# opt_splits = reg.choose_important_splits(0.2)
+# # check interactions
+# h_index = interaction.HIndex(data=X, model=model.predict, nof_instances=950)
+# # print(h_index.eval_pairwise(0, 1))
+# print(h_index.eval_one_vs_all(0))
+# h_index.plot(interaction_matrix=False, one_vs_all=True)
 #
-# transf = pythia.regions.DataTransformer(splits=opt_splits)
-# new_X = transf.transform(X)
+# # check interactions with REPID (dPDP based method)
+# repid_index = interaction.REPID(data=X, model=model.predict, model_jac=model.jacobian, nof_instances=950)
+# print(repid_index.eval_one_vs_all(0))
+# repid_index.plot()
+
+# find regions
+reg = pythia.regions.Regions(data=X, model=model.predict, model_jac=model.jacobian, cat_limit=25)
+reg.search_splits(nof_levels=2, nof_candidate_splits=20, criterion="rhale")
+opt_splits = reg.choose_important_splits(0.2)
+
+transf = pythia.regions.DataTransformer(splits=opt_splits)
+new_X = transf.transform(X)
+
+# split data
+seed = 21
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=seed)
+X_train_new, X_test_new, y_train_new, y_test_new = train_test_split(new_X, Y, test_size=0.20, random_state=seed)
+
+
+# fit a GAM to the transformed data
+gam = ExplainableBoostingRegressor(interactions=0)
+gam.fit(X_train_new, y_train_new)
+print(gam.score(X_test_new, y_test_new))
+
+# fit a GAM to the initial data
+ebr = ExplainableBoostingRegressor(interactions=0)
+ebr.fit(X_train, y_train)
+print(ebr.score(X_test, y_test))
+
+# fit a GAM with interactions to the initial data
+gam = ExplainableBoostingRegressor(interactions=2)
+gam.fit(X_train, y_train)
+print(gam.score(X_test, y_test))
+
+
+for i in range(len(ebr.term_scores_)):
+    plt.plot(ebr.term_scores_[i])
+    plt.show()
+
+from interpret import show
+show(ebr.explain_global())
+
 #
-# #
-# seed = 21
-# X_train, X_test, y_train, y_test = train_test_split(new_X, Y, test_size=0.20, random_state=seed)
-# ebr = ExplainableBoostingRegressor()
-# ebr.fit(X_train, y_train)
-# print(ebr.score(X_test, y_test))
-
-# for i in range(len(ebr.term_scores_)):
-#     plt.plot(ebr.term_scores_[i])
-#     plt.show()
+explanation = ebr.explain_global().visualize()
 
 
-# from interpret import show
-# show(ebr.explain_global())
+
+# explain
+fig, axs = plt.subplots(nrows=len(feature_names), figsize=(8, 6 * len(feature_names)))
+
+# Iterate over each feature
+for i, feature in enumerate(feature_names):
+    # Generate feature effect plot
+    feature_effect = ebr.explain().plot_feature_effect(feature)
+
+    # Set subplot details
+    axs[i].plot(feature_effect)
+    axs[i].set_xlabel(feature)
+    axs[i].set_ylabel('Effect')
+    axs[i].set_title(f'{feature} Effect')
+
+plt.tight_layout()
+plt.show()
 
 # # plot global effect
 # feat = 1
